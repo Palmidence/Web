382440944e8fa1d020b3b433184971e00c40041f
RubeGamerV <rbnvllgs@gmail.com>
2016-07-18 16:13:18 -0500

    Create english.php

---
+++ core/lang/english.php
@@ -0,0 +1,68 @@
+<?
+//Lenguajes
+ $motd = 'Working on it!';
+ 
+ $header[jugar] = 'Play';
+ $header[stats] = 'Stats':
+ $header[competitivo] = 'League';
+ $header[competitivo][equipos] = 'Teams';
+ $header[competitivo][torneos] = 'Tournaments';
+ $header[foro] = 'Forum';
+ $header[tienda] = 'Store';
+ $header[menu][perfil] = 'Profile';
+ $header[menu][alertas] = 'Alerts';
+ $header[menu][solicitudes] = 'Friendships';
+ $header[menu][reportes] = 'Reports';
+ $header[menu][apelaciones] = 'Appeals';
+ $header[menu][cuenta] = 'Account';
+ $header[menu][salir] = 'Logout';
+ 
+ $social[title] = 'Latest Announcements';
+ 
+ $marketing1[title] = 'ArenaPvP';
+ $marketing2[title] = 'KitMap';
+ $marketing3[title] = 'HCF':
+ 
+ $marketing1[desc] = 'Podras practicar con tus amigos o desconocidos para mejorar tu habilidad en PvP';
+ $marketing2[desc] = 'Practica con amigos para estar listo y jugar en HCF.';
+ $marketing3[desc] = 'Solo jugadores con experiencia se arriesgan a jugar en HardCore Factions';
+ 
+ $foro[main][nombre] = 'Forums';
+ $foro[main][desc] = 'Socialize with your friends';
+ 
+ $foro[bar][novedades] == 'News?';
+ $foro[bar][mistemas] == 'My Topics';
+ $foro[bar][anuncios] == 'Announcements';
+ $foro[bar][dudas] == 'Questions & Feedback';
+ $foro[bar][sub][comunidad] == 'Community';
+ $foro[bar][general] == 'General';
+ $foro[bar][sugerencias] == 'Suggestions';
+ $foro[bar][presentacion] == 'Introduce Yourself';
+ $foro[bar][offtopic] == 'OffTopic';
+ $foro[bar][sub][eventos] == 'Events';
+ $foro[bar][informacion] == 'Information';
+ $foro[bar][sub][otros] == 'Others';
+ $foro[bar][archivo] == 'Archive';
+ $foro[bar][solicitud] == 'Submissions';
+ $foro[table][tema] == 'Topic';
+ $foro[table][ultimo] == 'Last';
+ $foro[table][respuestas] == 'Posts';
+ $foro[table][vistas] == 'Views';
+ 
+ $stats[desc] = 'best players from the network';
+ $stats[table][usuario] = 'Player';
+ 
+ $cuenta[titulo] = 'Personal information';
+ $cuenta[titulo][desc] = 'share your information with others!';
+ 
+ $cuenta[uuid] = 'UUID (only here)';
+ $cuenta[otras][titulo] = 'Other configurations';
+ $cuenta[otras][solicitud] = 'Accept friend requests from other users';
+ $cuenta[otras][pais] = 'Show the country in my profile';
+ $cuenta[otras][server] = 'Show the server in my profile';
+ 
+ $cuenta[perfil][desc] = 'Use this box to add custom content to the bottom of your profile. You can use HTML and put anything in here, with a few exceptions:';
+ $cuenta[perfil][regla] = 'Follow our general rules for website behavior.';
+ $cuenta[perfil][consecuencia] = 'Dont try to break out of the box that contains the content.';
+ 
+?>

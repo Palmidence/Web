3058ea4dab4446f557cee20d483b898d8801d22e
RubeGamerV <rbnvllgs@gmail.com>
2016-07-18 15:11:27 -0500

    Create español.php

---
+++ core/lang/español.php
@@ -0,0 +1,16 @@
+<?
+//Lenguajes
+ $motd = ''
+ 
+ $marketing1[title] = ''
+ $marketing2[title] = ''
+ $marketing3[title] = ''
+ 
+ $marketing1[desc] = ''
+ $marketing2[desc] = ''
+ $marketing3[desc] = ''
+ 
+ $foro[main][nombre] = ''
+ $foro[main][desc] = ''
+
+?>

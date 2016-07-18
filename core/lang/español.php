e7512c34b48b8e0f4f18fad1df784dfe8bf9c6d6
RubeGamerV <rbnvllgs@gmail.com>
2016-07-18 15:57:44 -0500

    Update español.php
    Traducido al español

--- core/lang/español.php
+++ core/lang/español.php
@@ -2,68 +2,68 @@
 //Lenguajes
  $motd = '';
  
- $header[jugar] = '';
- $header[stats] = '':
- $header[competitivo] = '';
- $header[competitivo][equipos] = '';
- $header[competitivo][torneos] = '';
- $header[foro] = '';
- $header[tienda] = '';
- $header[menu][perfil] = '';
- $header[menu][alertas] = '';
- $header[menu][solicitudes] = '';
- $header[menu][reportes] = '';
- $header[menu][apelaciones] = '';
- $header[menu][cuenta] = '';
- $header[menu][salir] = '';
+ $header[jugar] = 'Jugar';
+ $header[stats] = 'Stats':
+ $header[competitivo] = 'Competitivo';
+ $header[competitivo][equipos] = 'Equipos';
+ $header[competitivo][torneos] = 'Torneos';
+ $header[foro] = 'Foro';
+ $header[tienda] = 'Tienda';
+ $header[menu][perfil] = 'Perfil';
+ $header[menu][alertas] = 'Alertas';
+ $header[menu][solicitudes] = 'Solicitudes';
+ $header[menu][reportes] = 'Reportes';
+ $header[menu][apelaciones] = 'Apelaciones';
+ $header[menu][cuenta] = 'Cuenta';
+ $header[menu][salir] = 'Salir';
  
- $marketing1[title] = '';
- $marketing2[title] = '';
- $marketing3[title] = '':
+ $marketing1[title] = 'ArenaPvP';
+ $marketing2[title] = 'KitMap';
+ $marketing3[title] = 'HCF':
  
- $marketing1[desc] = '';
- $marketing2[desc] = '';
- $marketing3[desc] = '';
+ $marketing1[desc] = 'Podras practicar con tus amigos o desconocidos para mejorar tu habilidad en PvP';
+ $marketing2[desc] = 'Practica con amigos para estar listo y jugar en HCF.';
+ $marketing3[desc] = 'Solo jugadores con experiencia se arriesgan a jugar en HardCore Factions';
  
- $foro[main][nombre] = '';
- $foro[main][desc] = '';
+ $foro[main][nombre] = 'Foros';
+ $foro[main][desc] = 'Socializa con la comunidad';
  
- $foro[bar][novedades] == '';
- $foro[bar][mistemas] == '';
- $foro[bar][anuncios] == '';
- $foro[bar][dudas] == '';
- $foro[bar][sub][comunidad] == '';
- $foro[bar][general] == '';
- $foro[bar][sugerencias] == '';
- $foro[bar][presentacion] == '';
- $foro[bar][offtopic] == '';
- $foro[bar][sub][eventos] == '';
- $foro[bar][informacion] == '';
- $foro[bar][sub][otros] == '';
- $foro[bar][archivo] == '';
- $foro[bar][solicitud] == '';
+ $foro[bar][novedades] == 'Novedades?';
+ $foro[bar][mistemas] == 'Mis temas';
+ $foro[bar][anuncios] == 'Anuncios';
+ $foro[bar][dudas] == 'Dudas';
+ $foro[bar][sub][comunidad] == 'Comunidad';
+ $foro[bar][general] == 'General';
+ $foro[bar][sugerencias] == 'Sugerencia';
+ $foro[bar][presentacion] == 'Presentacion';
+ $foro[bar][offtopic] == 'OffTopic';
+ $foro[bar][sub][eventos] == 'Eventos';
+ $foro[bar][informacion] == 'Informacion';
+ $foro[bar][sub][otros] == 'Otros';
+ $foro[bar][archivo] == 'Archivo';
+ $foro[bar][solicitud] == 'Solicitud';
 
- $foro[table][tema] == '';
- $foro[table][ultimo] == '';
- $foro[table][respuestas] == '';
- $foro[table][vistas] == '';
+ $foro[table][tema] == 'Tema';
+ $foro[table][ultimo] == 'Ultimo';
+ $foro[table][respuestas] == 'Respuestas';
+ $foro[table][vistas] == 'Vistas';
  
- $foro[topic][foro] = '';
- $foro[topic][desc] = '';
+ $foro[topic][foro] = 'Foros';
+ $foro[topic][desc] = 'haz crecer la comunidad';
  
- $stats[desc] = '';
- $stats[table][usuario] = '';
+ $stats[desc] = 'los mejores jugadores de la network';
+ $stats[table][usuario] = 'Usuario';
  
- $cuenta[titulo] = '';
- $cuenta[titulo][desc] = '';
+ $cuenta[titulo] = 'Añadir informacion';
+ $cuenta[titulo][desc] = 'comparte tu informacion para que otros usuarios la miren!';
  
- $cuenta[uuid] = '';
- $cuenta[otras][titulo] = '';
- $cuenta[otras][solicitud] = '';
- $cuenta[otras][pais] = '';
- $cuenta[otras][server] = '';
+ $cuenta[uuid] = 'UUID (solo en el server)';
+ $cuenta[otras][titulo] = 'Otras configuraciones';
+ $cuenta[otras][solicitud] = 'Aceptar solicitudes';
+ $cuenta[otras][pais] = 'Mostrar paises';
+ $cuenta[otras][server] = 'Mostrar servidor donde estoy online';
  
- $cuenta[perfil][desc] = '';
- $cuenta[perfil][regla] = '';
- $cuenta[perfil][consecuencia] = '';
+ $cuenta[perfil][desc] = 'Usa esta seccion para poner una descripcion personalizada en tu perfil. Puedes usar HTML, con algunas excepciónes:';
+ $cuenta[perfil][regla] = 'No trates de romper la seccion con algun codigo mal escrito.';
+ $cuenta[perfil][consecuencia] = 'No trates de romper la seccion con algun codigo mal escrito.';
 ?>

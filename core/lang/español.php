a0d6bee81a00a0de9c3c9d5976086551e662ea17
RubeGamerV <rbnvllgs@gmail.com>
2016-07-18 15:47:10 -0500

    Update español.php

--- core/lang/español.php
+++ core/lang/español.php
@@ -1,16 +1,69 @@
 <?
 //Lenguajes
- $motd = ''
+ $motd = '';
  
- $marketing1[title] = ''
- $marketing2[title] = ''
- $marketing3[title] = ''
+ $header[jugar] = '';
+ $header[stats] = '':
+ $header[competitivo] = '';
+ $header[competitivo][equipos] = '';
+ $header[competitivo][torneos] = '';
+ $header[foro] = '';
+ $header[tienda] = '';
+ $header[menu][perfil] = '';
+ $header[menu][alertas] = '';
+ $header[menu][solicitudes] = '';
+ $header[menu][reportes] = '';
+ $header[menu][apelaciones] = '';
+ $header[menu][cuenta] = '';
+ $header[menu][salir] = '';
  
- $marketing1[desc] = ''
- $marketing2[desc] = ''
- $marketing3[desc] = ''
+ $marketing1[title] = '';
+ $marketing2[title] = '';
+ $marketing3[title] = '':
  
- $foro[main][nombre] = ''
- $foro[main][desc] = ''
+ $marketing1[desc] = '';
+ $marketing2[desc] = '';
+ $marketing3[desc] = '';
+ 
+ $foro[main][nombre] = '';
+ $foro[main][desc] = '';
+ 
+ $foro[bar][novedades] == '';
+ $foro[bar][mistemas] == '';
+ $foro[bar][anuncios] == '';
+ $foro[bar][dudas] == '';
+ $foro[bar][sub][comunidad] == '';
+ $foro[bar][general] == '';
+ $foro[bar][sugerencias] == '';
+ $foro[bar][presentacion] == '';
+ $foro[bar][offtopic] == '';
+ $foro[bar][sub][eventos] == '';
+ $foro[bar][informacion] == '';
+ $foro[bar][sub][otros] == '';
+ $foro[bar][archivo] == '';
+ $foro[bar][solicitud] == '';
 
+ $foro[table][tema] == '';
+ $foro[table][ultimo] == '';
+ $foro[table][respuestas] == '';
+ $foro[table][vistas] == '';
+ 
+ $foro[topic][foro] = '';
+ $foro[topic][desc] = '';
+ 
+ $stats[desc] = '';
+ $stats[table][usuario] = '';
+ 
+ $cuenta[titulo] = '';
+ $cuenta[titulo][desc] = '';
+ 
+ $cuenta[uuid] = '';
+ $cuenta[otras][titulo] = '';
+ $cuenta[otras][solicitud] = '';
+ $cuenta[otras][pais] = '';
+ $cuenta[otras][server] = '';
+ 
+ $cuenta[perfil][desc] = '';
+ $cuenta[perfil][regla] = '';
+ $cuenta[perfil][consecuencia] = '';
 ?>

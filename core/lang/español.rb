235cddf4a01dc8d255a11ca597a14680d78f9490
RubeGamerV <rbnvllgs@gmail.com>
2016-07-31 11:20:38 -0600

    Switch to ruby 2

---
+++ core/lang/español.rb
@@ -0,0 +1,96 @@
+define("lang", "Español");
+        		define("motd", "La competitividad al alcance de tu mano");
+        		define("motd_button", "Jugar!");
+
+                define("header_play", "Jugar");
+                define("header_stats", "Stats");
+                define("header_competitivo", "Competitivo");
+                define("header_competitivo_equipos", "Equipos");
+                define("header_competitivo_torneos", "Torneos");
+                define("header_foro", "Foro");
+                define("header_tienda", "Tienda");
+                define("header_menu_cuenta_entrar", "Entrar");
+                define("header_menu_cuenta_registrar", "Registrar");
+                define("header_menu_perfil", "Perfil");
+                define("header_menu_alertas", "Alertas");
+                define("header_menu_solicitudes", "Solicitudes");
+                define("header_menu_reportes", "Reportes");
+                define("header_menu_apelaciones", "Apelaciones");
+                define("header_menu_cuenta", "Cuenta");
+                define("header_menu_salir", "Salir");
+                 
+                define("social", "Ultimos anuncios");
+ 
+                define("foro_main", "Foros");
+                define("foro_main_desc", "Socializa con la comunidad");
+ 
+                define("foro_bar_news", "Novedades?");
+				define("foro_bar_mistemas", "Mis temas");
+				define("foro_bar_anuncios", "Anuncios");
+				define("foro_bar_dudas", "Preguntale al staff");
+				define("foro_bar_sub_comunidad", "Comunidad");
+				define("foro_bar_general", "General");
+				define("foro_bar_sugerencias", "Sugerencias");
+				define("foro_bar_presentacion", "Presentacion");
+				define("foro_bar_cc", "Contenido creativo");
+				define("foro_bar_offtopic", "OffTopic");
+				define("foro_bar_sub_mapmaking", "Map Making");
+				define("foro_bar_mapasinfo", "Informacion");
+				define("foro_bar_mapas", "Mapas");
+				define("foro_bar_sub_eventos", "Eventos");
+				define("foro_bar_informacion", "Informacion");
+				define("foro_bar_lft", "Buscando equipo");
+				define("foro_bar_lfm", "Buscando miembro");
+				define("foro_bar_sub_otros", "Otros");
+				define("foro_bar_archivo", "Archivo");
+				define("foro_bar_solicitud", "Solicitudes");
+				define("foro_bar_online", "Usuarios online");
+
+
+                
+				define("foro_table_tema", "Tema");
+				define("foro_table_ultimo", "Ultimo");
+				define("foro_table_respuestas", "Posts");
+				define("foro_table_vistas", "Visitas");
+                
+                define("foro_topic_button_archivar", "Archivar");
+				define("foro_topic_button_desarchivar", "Desarchivar");
+				define("foro_topic_button_bloquear", "Bloquear");
+				define("foro_topic_button_desbloquear", "Desbloquear");
+				define("foro_topic_button_borrar", "Borrar");
+				define("foro_topic_button_responder", "Responder");
+				define("foro_topic_button_cancelar", "Cancelar");
+				define("foro_topic_button_editar", "Editar");
+				define("foro_topic_button_accion", "Accion");
+				define("foro_topic_button_accion_borrar", "Borrar");
+				define("foro_topic_button_accion_editar", "Editar");
+ 
+                define("stats_desc", "mejores jugadores de la network");
+                define("stats_table", "Jugador");
+ 
+                define("cuenta_titulo", "Informacion");
+				define("cuenta_titulo_desc", "comparte tu informacion con otros");
+ 
+                define("cuenta_otras_titulo", "Otros");
+				define("cuenta_otras_solicitud", "Aceptar solicitudes de otros jugadores");
+				define("cuenta_otras_pais", "Mostrar pais");
+				define("cuenta_otras_server", "Mostrar server donde estoy conectado");
+ 
+                define("cuenta_perfil_desc", "Usa esta seccion para poner una descripcion personalizada en tu perfil. Puedes usar HTML, con algunas excepciónes:");
+                define("cuenta_perfil_regla", "Si no sigue las anteriores indicaciones puede que su perfil se personalizado sea borrado y bloqueado permanente.");
+                define("cuenta_perfil_consecuencia", "No trates de romper la seccion con algun codigo mal escrito.");
+                define("cuenta_perfil_button_update", "Actualizar");
+				
+				define("footer_politicas", "Politicas");
+				define("footer_politicas_reglas", "Reglas");
+				define("footer_politicas_terminos", "Terminos de uso");
+				
+				define("footer_herramientas", "Herramientas");
+				define("footer_herramientas_reportar", "Reportar a un jugador");
+				define("footer_herramientas_apelar", "Apelar infraccion");
+				define("footer_herramientas_infracciones", "Infracciones");
+				
+				define("footer_organizacion", "Organizacion");
+				define("footer_organizacion_staff", "Staff");
+				define("footer_organizacion_desarrollo", "Desarrollo");
+				define("footer_organizacion_bugs", "Reportar un bug");

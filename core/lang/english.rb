1f4093218eab39e17ce0fc5d54ec1683f0127db4
RubeGamerV <rbnvllgs@gmail.com>
2016-07-31 11:19:17 -0600

    Switch to ruby

---
+++ core/lang/english.rb
@@ -0,0 +1,95 @@
+    define("lang", "English");
+    define("motd", "Working on it!");
+    define("motd_button", "Play!");
+
+    define("header_play", "Play");
+    define("header_stats", "Stats");
+    define("header_competitivo", "League");
+    define("header_competitivo_equipos", "Teams");
+    define("header_competitivo_torneos", "Tournaments");
+    define("header_foro", "Forum");
+    define("header_tienda", "Shop");
+    define("header_menu_cuenta_entrar", "Login");
+    define("header_menu_cuenta_registrar", "Register");
+    define("header_menu_perfil", "Profile");
+    define("header_menu_alertas", "Alerts");
+    define("header_menu_solicitudes", "Friendships");
+    define("header_menu_reportes", "Reports");
+    define("header_menu_apelaciones", "Appeals");
+    define("header_menu_cuenta", "Account");
+    define("header_menu_salir", "Logout");
+                 
+    define("social", "Last announces");
+ 
+    define("foro_main", "Forums");
+    define("foro_main_desc", "Socialize with your friends");
+ 
+    define("foro_bar_news", "News?");
+				define("foro_bar_mistemas", "My topics");
+				define("foro_bar_anuncios", "Announcements");
+				define("foro_bar_dudas", "Ask to admins");
+				define("foro_bar_sub_comunidad", "Community");
+				define("foro_bar_general", "General");
+				define("foro_bar_presentacion", "Introduce yourself");
+				define("foro_bar_cc", "Creative content");
+				define("foro_bar_sugerencias", "Suggestions");
+				define("foro_bar_offtopic", "OffTopic");
+				define("foro_bar_sub_mapmaking", "Map Making");
+				define("foro_bar_mapasinfo", "Information");
+				define("foro_bar_mapas", "Maps");
+				define("foro_bar_sub_eventos", "Events");
+				define("foro_bar_informacion", "Information");
+				define("foro_bar_lft", "Looking for Team");
+				define("foro_bar_lfm", "Looking for Member");
+				define("foro_bar_sub_otros", "Others");
+				define("foro_bar_archivo", "Archive");
+				define("foro_bar_solicitud", "Submissions");
+				define("foro_bar_online", "Online users");
+
+                
+				define("foro_table_tema", "Topic");
+				define("foro_table_ultimo", "Last");
+				define("foro_table_respuestas", "Posts");
+				define("foro_table_vistas", "Views");
+                
+    define("foro_topic_button_archivar", "Archive");
+				define("foro_topic_button_desarchivar", "Unrchive");
+				define("foro_topic_button_bloquear", "Lock");
+				define("foro_topic_button_desbloquear", "Unlock");
+				define("foro_topic_button_borrar", "Delete");
+				define("foro_topic_button_responder", "Reply");
+				define("foro_topic_button_cancelar", "Cancel");
+				define("foro_topic_button_editar", "Edit");
+				define("foro_topic_button_accion", "Action");
+				define("foro_topic_button_accion_borrar", "Hide");
+				define("foro_topic_button_accion_editar", "Edit");
+ 
+    define("stats_desc", "best players from the network");
+    define("stats_table", "Player");
+ 
+    define("cuenta_titulo", "Information");
+				define("cuenta_titulo_desc", "share your info to other players");
+ 
+    define("cuenta_otras_titulo", "Others");
+				define("cuenta_otras_solicitud", "Accept friend requests from other players");
+				define("cuenta_otras_pais", "Show country");
+				define("cuenta_otras_server", "Show server online");
+ 
+    define("cuenta_perfil_desc", "Use this box to add custom content to the bottom of your profile. You can use HTML and put anything in here, with a few exceptions:");
+    define("cuenta_perfil_regla", "Follow our general rules for website behavior.");
+    define("cuenta_perfil_consecuencia", "Dont try to break out of the box that contains the content.");
+    define("cuenta_perfil_button_update", "Update");
+				
+				define("footer_politicas", "Policies");
+				define("footer_politicas_reglas", "Rules");
+				define("footer_politicas_terminos", "Terms of Service");
+				
+				define("footer_herramientas", "Moderation");
+				define("footer_herramientas_reportar", "Report a player");
+				define("footer_herramientas_apelar", "Appeal a punishment");
+				define("footer_herramientas_infracciones", "Punishments");
+				
+				define("footer_organizacion", "Organization");
+				define("footer_organizacion_staff", "Staff");
+				define("footer_organizacion_desarrollo", "Development logs");
+				define("footer_organizacion_bugs", "Report a bug");

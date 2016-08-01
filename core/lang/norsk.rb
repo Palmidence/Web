6698ac4a38d05f9c962ecdd36a299fbab3b2899b
Johannes Theigen <johannes.theigen@gmail.com>
2016-08-01 21:25:09 +0200

    Add Norwegian Language
    for now I'll call the define lang norsk which means norwegian if that's incorrect in ruby I'll fix it and the file will be named norsk.rb.
    Hopefully I am doing this correct.

---
+++ core/lang/norsk.rb
@@ -0,0 +1,95 @@
+  define("lang", "Norsk");
+    define("motd", "Jobber med det!");
+    define("motd_button", "Spill!");
+
+    define("header_play", "Spill");
+    define("header_stats", "Statistikk");
+    define("header_competitivo", "Liga");
+    define("header_competitivo_equipos", "Lag");
+    define("header_competitivo_torneos", "Turneringer");
+    define("header_foro", "Forum");
+    define("header_tienda", "Butikk");
+    define("header_menu_cuenta_entrar", "Logg inn");
+    define("header_menu_cuenta_registrar", "Registrer");
+    define("header_menu_perfil", "Profil");
+    define("header_menu_alertas", "Vennskap");
+    define("header_menu_solicitudes", "Vennskap");
+    define("header_menu_reportes", "Rapporter");
+    define("header_menu_apelaciones", "Anker");
+    define("header_menu_cuenta", "Konto");
+    define("header_menu_salir", "Logg ut");
+                 
+    define("social", "Siste kunngjøringer");
+ 
+    define("foro_main", "Forumet");
+    define("foro_main_desc", "Sosialiser deg med vennene dine");
+ 
+    define("foro_bar_news", "Nyheter?");
+				define("foro_bar_mistemas", "Mine emner");
+				define("foro_bar_anuncios", "Kunngjøringer");
+				define("foro_bar_dudas", "Spør administratorene");
+				define("foro_bar_sub_comunidad", "Samfunnet");
+				define("foro_bar_general", "Generelt");
+				define("foro_bar_presentacion", "Introduser deg selv");
+				define("foro_bar_cc", "kreativt innhold");
+				define("foro_bar_sugerencias", "Forslag");
+				define("foro_bar_offtopic", "OffTopic");
+				define("foro_bar_sub_mapmaking", "Kart Laging");
+				define("foro_bar_mapasinfo", "Informasjon");
+				define("foro_bar_mapas", "Kart");
+				define("foro_bar_sub_eventos", "Arrangementer");
+				define("foro_bar_informacion", "Informasjon");
+				define("foro_bar_lft", "Ser etter lag");
+				define("foro_bar_lfm", "Ser etter medlemer");
+				define("foro_bar_sub_otros", "Andre");
+				define("foro_bar_archivo", "Arkiv");
+				define("foro_bar_solicitud", "Innsendelser");
+				define("foro_bar_online", "Påloggede brukere");
+
+                
+				define("foro_table_tema", "Emne");
+				define("foro_table_ultimo", "Siste");
+				define("foro_table_respuestas", "Innlegg");
+				define("foro_table_vistas", "Visninger");
+                
+    define("foro_topic_button_archivar", "Arkiver");
+				define("foro_topic_button_desarchivar", "Dearkiver");
+				define("foro_topic_button_bloquear", "Lås");
+				define("foro_topic_button_desbloquear", "Lås opp");
+				define("foro_topic_button_borrar", "Slett");
+				define("foro_topic_button_responder", "Svar");
+				define("foro_topic_button_cancelar", "Avbryt");
+				define("foro_topic_button_editar", "Rediger");
+				define("foro_topic_button_accion", "Handling");
+				define("foro_topic_button_accion_borrar", "Gjem");
+				define("foro_topic_button_accion_editar", "Rediger");
+ 
+    define("stats_desc", "beste spiller fra nettverket");
+    define("stats_table", "Spiller");
+ 
+    define("cuenta_titulo", "Informasjon");
+				define("cuenta_titulo_desc", "del din info til andre spillere");
+ 
+    define("cuenta_otras_titulo", "Andre");
+				define("cuenta_otras_solicitud", "Aksepter venneforespørsler fra andre spillere");
+				define("cuenta_otras_pais", "Vis land");
+				define("cuenta_otras_server", "Vis server på nettet");
+ 
+    define("cuenta_perfil_desc", "Bruk denne boksen til å legge til egendefinert innhold til bunnen av profilen din. Du kan bruke HTML og putte hva som helst her, med noen få unntak:");
+    define("cuenta_perfil_regla", "Følg våre generelle regler for nettside oppførsel.");
+    define("cuenta_perfil_consecuencia", "Ikke prøv å bryt ut av boksen som inneholder innhold.");
+    define("cuenta_perfil_button_update", "Oppdater");
+				
+				define("footer_politicas", "Retningslinjer");
+				define("footer_politicas_reglas", "Regler");
+				define("footer_politicas_terminos", "Vilkår for bruk");
+				
+				define("footer_herramientas", "Moderasjon");
+				define("footer_herramientas_reportar", "Rapporter en spiller");
+				define("footer_herramientas_apelar", "Anke en straff");
+				define("footer_herramientas_infracciones", "Straffer");
+				
+				define("footer_organizacion", "Organisasjon");
+				define("footer_organizacion_staff", "Personalet");
+				define("footer_organizacion_desarrollo", "Utviklingslogger");
+				define("footer_organizacion_bugs", "Rapporter en feil");

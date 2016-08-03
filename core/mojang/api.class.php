6a4174c16ed0383879d4383c1506e84b777fee5a
RubeGamerV <rbnvllgs@gmail.com>
2016-08-03 09:20:21 -0600

    First step for Mojang API

--- core/mojang/api.class.php
+++ core/mojang/api.class.php
@@ -1,1 +1,18 @@
-
+<?
+class API{
+  public static function getStatus() {
+    $json = self::fetchJson('https://status.mojang.com/check');
+        if (is_array($json)) {
+            $status = array();
+            foreach ($json as $array) {
+                if (!empty($array)) {
+                    list($key, $value) = each($array);
+                    $status[$key] = $value;
+                }
+            }
+            return $status;
+        }
+        return false;
+  }
+}
+?>

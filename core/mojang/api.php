30d61254dca50a494101c82f36459daa75178f71
RubeGamerV <rbnvllgs@gmail.com>
2016-08-02 19:31:48 -0600

    Update api.php

--- core/mojang/api.php
+++ core/mojang/api.php
@@ -1,1 +1,22 @@
+<?
+function profile($username)
+{
+  $json = file_get_contents('https://api.mojang.com/users/profiles/minecraft/' . $username);
+  if(!empty($json)){
+    $data = json_decode($json, true);
+    if(is_array($data) and !empty($data)){
+      return $data;
+    }
+  }
+}
 
+function fix_uuid($uuid)
+{
+  if(is_string($uuid)){
+    $minified = str_replace('-', '', $uuid);
+    if(strlen($minified) === 32){
+      return $minified;
+    }
+  }
+}
+?>

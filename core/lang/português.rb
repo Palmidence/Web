2d0f3f795630fb8abac7f49268c1867bcab50b7f
RubeGamerV <rbnvllgs@gmail.com>
2016-08-03 13:01:59 -0600

    Update português.rb

--- core/lang/português.rb
+++ core/lang/português.rb
@@ -1,6 +1,6 @@
-        		define("lang", "Portuguese");
-        		define("motd", "Competitividade ao seu alcance");
-        		define("motd_button", "Jogar!");
+        	define("lang", "Portuguese");
+        	define("motd", "Competitividade ao seu alcance");
+        	define("motd_button", "Jogar!");
 
                 define("header_play", "Jogar");
                 define("header_stats", "Stats");
@@ -25,70 +25,70 @@
                 define("foro_main_desc", "Socializar com os seus amigos");
  
                 define("foro_bar_news", "Notícia?");
-				define("foro_bar_mistemas", "Meus tópicos");
-				define("foro_bar_anuncios", "Anúncio");
-				define("foro_bar_dudas", "Peça para os administradores");
-				define("foro_bar_sub_comunidad", "Comunidade");
-				define("foro_bar_general", "Geral");
-				define("foro_bar_sugerencias", "Sugestões");
-				define("foro_bar_presentacion", "Apresente-se");
-				define("foro_bar_cc", "Conteúdo criativo");
-				define("foro_bar_offtopic", "Fora do assunto");
-				define("foro_bar_sub_mapmaking", "Fazendo mapa");
-				define("foro_bar_mapasinfo", "Em formação");
-				define("foro_bar_mapas", "Mapas");
-				define("foro_bar_sub_eventos", "Eventos");
-				define("foro_bar_informacion", "Em formação");
-				define("foro_bar_lft", "Procurando por equipe");
-				define("foro_bar_lfm", "Procurando por membro");
-				define("foro_bar_sub_otros", "Outras");
-				define("foro_bar_archivo", "Arquivo");
-				define("foro_bar_solicitud", "Aplicação");
-				define("foro_bar_online", "Usuários online");
+		define("foro_bar_mistemas", "Meus tópicos");
+		define("foro_bar_anuncios", "Anúncio");
+		define("foro_bar_dudas", "Peça para os administradores");
+		define("foro_bar_sub_comunidad", "Comunidade");
+		define("foro_bar_general", "Geral");
+		define("foro_bar_sugerencias", "Sugestões");
+		define("foro_bar_presentacion", "Apresente-se");
+		define("foro_bar_cc", "Conteúdo criativo");
+		define("foro_bar_offtopic", "Fora do assunto");
+		define("foro_bar_sub_mapmaking", "Fazendo mapa");
+		define("foro_bar_mapasinfo", "Em formação");
+		define("foro_bar_mapas", "Mapas");
+		define("foro_bar_sub_eventos", "Eventos");
+		define("foro_bar_informacion", "Em formação");
+		define("foro_bar_lft", "Procurando por equipe");
+		define("foro_bar_lfm", "Procurando por membro");
+		define("foro_bar_sub_otros", "Outras");
+		define("foro_bar_archivo", "Arquivo");
+		define("foro_bar_solicitud", "Aplicação");
+		define("foro_bar_online", "Usuários online");
                 
-				define("foro_table_tema", "Tópic");
-				define("foro_table_ultimo", "Último");
-				define("foro_table_respuestas", "Postagens");
-				define("foro_table_vistas", "Visualizações");
+		define("foro_table_tema", "Tópic");
+		define("foro_table_ultimo", "Último");
+		define("foro_table_respuestas", "Postagens");
+		define("foro_table_vistas", "Visualizações");
                 
                 define("foro_topic_button_archivar", "Arquive");
-				define("foro_topic_button_desarchivar", "Desarquivar");
-				define("foro_topic_button_bloquear", "Trancar");
-				define("foro_topic_button_desbloquear", "Destrancar");
-				define("foro_topic_button_borrar", "Excluir");
-				define("foro_topic_button_responder", "Responder");
-				define("foro_topic_button_cancelar", "Cancelar");
-				define("foro_topic_button_editar", "Editar");
-				define("foro_topic_button_accion", "Açao");
-				define("foro_topic_button_accion_borrar", "Excluir");
-				define("foro_topic_button_accion_editar", "Editar");
+		define("foro_topic_button_desarchivar", "Desarquivar");
+		define("foro_topic_button_bloquear", "Trancar");
+		define("foro_topic_button_desbloquear", "Destrancar");
+		define("foro_topic_button_borrar", "Excluir");
+		define("foro_topic_button_responder", "Responder");
+		define("foro_topic_button_cancelar", "Cancelar");
+		define("foro_topic_button_editar", "Editar");
+		define("foro_topic_button_accion", "Açao");
+		define("foro_topic_button_accion_borrar", "Excluir");
+		define("foro_topic_button_accion_editar", "Editar");
  
                 define("stats_desc", "Melhores jogadores da network");
                 define("stats_table", "Jogador");
  
                 define("cuenta_titulo", "Em formação");
-				define("cuenta_titulo_desc", "Compartilhar suas informações com outros");
+		define("cuenta_titulo_desc", "Compartilhar suas informações com outros");
  
                 define("cuenta_otras_titulo", "Outros");
-				define("cuenta_otras_solicitud", "Aceitar solicitação de amizade");
-				define("cuenta_otras_pais", "Mostre seu país");
-				define("cuenta_otras_server", "Mostre seu servidor online");
+		define("cuenta_otras_solicitud", "Aceitar solicitação de amizade");
+		define("cuenta_otras_pais", "Mostre seu país");
+		define("cuenta_otras_server", "Mostre seu servidor online");
  
                 define("cuenta_perfil_desc", "Use esta seção para adicionar uma descrição personalizada em seu perfil. Você pode usar HTML, com algumas exceções:");
                 define("cuenta_perfil_regla", "O não cumprimento das indicações acima pode ser personalizado perfil é excluído e permanentemente bloqueado.");
                 define("cuenta_perfil_consecuencia", "Não tente quebrar a seção de código com alguma incorreta.");
                 define("cuenta_perfil_button_update", "Atualizar");
 				
-				define("footer_politicas", "Políticas");
-				define("footer_politicas_reglas", "Regras");
-				define("footer_politicas_terminos", "Termos de serviço");
+		define("footer_politicas", "Políticas");
+		define("footer_politicas_reglas", "Regras");
+		define("footer_politicas_terminos", "Termos de serviço");
 				
-				define("footer_herramientas", "Moderação");
-				define("footer_herramientas_reportar", "Denunciar um jogador");
-				define("footer_herramientas_apelar", "Recorrer da punição");
-				define("footer_herramientas_infracciones", "Punições");
+		define("footer_herramientas", "Moderação");
+		define("footer_herramientas_reportar", "Denunciar um jogador");
+		define("footer_herramientas_apelar", "Recorrer da punição");
+		define("footer_herramientas_infracciones", "Punições");
 				
-				define("footer_organizacion", "Organização");
-				define("footer_organizacion_staff", "Funcionários");
-				define("footer_organizacion_desarrollo", "Toras de desenvolvimento");
-				define("footer_organizacion_bugs", "Reportar um erro");
+		define("footer_organizacion", "Organização");
+		define("footer_organizacion_staff", "Funcionários");
+		define("footer_organizacion_desarrollo", "Toras de desenvolvimento");
+		define("footer_organizacion_bugs", "Reportar um erro");
